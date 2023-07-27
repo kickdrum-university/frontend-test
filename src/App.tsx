8eda823199e153c30b08a1d44e35fcc5d93d88dd
yadavpreeti00 <yadavpreeti5668@gmail.com>
2023-07-27 17:12:38 +0530

    exer-4

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload exercise-4.
+          Edit <code>src/App.tsx</code> and save to reload exercise-4-react.
         </p>
         <a
           className="App-link"
