ec1b9343be0470169907543997b666305417ccd2
yadavpreeti00 <yadavpreeti5668@gmail.com>
2023-08-07 13:14:39 +0530

    e

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reloadd exercise-2.
+          Edit <code>src/App.tsx</code> and save o reloadd exercise-2.
         </p>
         <a
           className="App-link"
