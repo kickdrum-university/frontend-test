796edc60074874748489f4797a27bcdc0dee0821
yadavpreeti00 <yadavpreeti5668@gmail.com>
2023-08-07 11:40:15 +0530

    p

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload exercise--reacte new
+          Edit <code>src/App.tsx</code> d save to reload exercise--reacte new
           bucket load image new.
         </p>
         <a
